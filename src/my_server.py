7dc0c11c3246dc3f9d6a19241c553e93d27d971d
kgvinay <kgvinay.123@gmail.com>
2026-02-09 23:33:29 +0530

    Adding Two numbers

--- src/my_server.py
+++ src/my_server.py
@@ -1,10 +1,11 @@
 from fastmcp import FastMCP
 
-mcp = FastMCP("My MCP Server")
+mcp = FastMCP("Demo 🚀")
 
 @mcp.tool
-def greet(name: str) -> str:
-    return f"Hello, {name}!"
+def add(a: int, b: int) -> int:
+    """Add two numbers"""
+    return a + b
 
 if __name__ == "__main__":
     mcp.run()
